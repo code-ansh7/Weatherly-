581bd392b8afa1fd4c56b95ebb266a50bd5c71c8
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-09-20 11:55:20 +0545

    🎨 feat: add Weatherly onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weatherly/screens/onboarding_screen.dart';
 import 'package:weatherly/screens/splash_screen.dart';
 
 void main() {
@@ -12,7 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      // home: SplashScreen(),
+      home: OnboardingScreen(),
     );
   }
 }
---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF061B33),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            children: [
+              const SizedBox(height: 70),
+
+              // Weather Icons
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(
+                    Icons.cloud,
+                    size: 90,
+                    color: Colors.cyanAccent,
+                  ),
+                  const Icon(
+                    Icons.wb_sunny,
+                    size: 45,
+                    color: Colors.amber,
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 40),
+
+              // Heading
+              const Text(
+                "Welcome to\nWeatherly",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  height: 1.2,  //Space b/w heading Gaps
+                ),
+              ),
+
+              const SizedBox(height: 15),
+
+              // Description
+              const Text(
+                "Get real-time weather information\n"
+                "for every place you care about.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white70,
+                  height: 1.5,
+                ),
+              ),
+
+
+              const Spacer(),
+
+              // Get Started Button
+              SizedBox(
+                width: double.infinity,
+                height: 54,
+                child: ElevatedButton(
+                  onPressed: () {
+           
+                  },
+                  style: ElevatedButton.styleFrom(
+                    elevation: 10,
+                    backgroundColor: Colors.cyanAccent,
+                    foregroundColor: const Color(0xFF061B33),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                  child: const Text(
+                    "Get Started →",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 12),
+
+              // Skip Button
+              TextButton(
+                onPressed: () {
+                  // Navigation will be added later
+                },
+                child: const Text(
+                  "Skip",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white70,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
