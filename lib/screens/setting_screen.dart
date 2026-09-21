07d1e62ebdce6b58279658e41298c890c5edb37c
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-09-21 16:38:15 +0545

    🎨 feat: add theme selector to settings

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -122,17 +122,20 @@ class _SettingsScreenState extends State<SettingsScreen> {
                           ),
 
                           child: Center(
-                            child: Text("°C",
+                            child: Text(
+                              "°C",
                               style: TextStyle(
-                                color: isCelsius ? Color(0xFF061B33) : Colors.white70
+                                color: isCelsius
+                                    ? Color(0xFF061B33)
+                                    : Colors.white70,
                               ),
-                            )
+                            ),
                           ),
                         ),
                       ),
                     ),
 
-                    SizedBox(width: 7,),
+                    SizedBox(width: 7),
 
                     Expanded(
                       child: InkWell(
@@ -152,11 +155,14 @@ class _SettingsScreenState extends State<SettingsScreen> {
                           ),
 
                           child: Center(
-                            child: Text("°F",
+                            child: Text(
+                              "°F",
                               style: TextStyle(
-                                color: !isCelsius ? Color(0xFF061B33) : Colors.white70
+                                color: !isCelsius
+                                    ? Color(0xFF061B33)
+                                    : Colors.white70,
                               ),
-                            )
+                            ),
                           ),
                         ),
                       ),
@@ -164,6 +170,150 @@ class _SettingsScreenState extends State<SettingsScreen> {
                   ],
                 ),
               ),
+
+              const SizedBox(height: 30),
+
+              //Theme Heading
+              Row(
+                children: [
+                  const Icon(
+                    Icons.palette_outlined,
+                    color: Colors.cyanAccent,
+                    size: 21,
+                  ),
+
+                  const SizedBox(width: 8),
+
+                  const Text(
+                    "Theme",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 12),
+
+              // Theme Container
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(7),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.10),
+                  borderRadius: BorderRadius.circular(18),
+                  border: Border.all(color: Colors.white.withOpacity(0.12)),
+                ),
+
+                child: Row(
+                  children: [
+                    // Light
+                    Expanded(
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(13),
+                        onTap: () {
+                          setState(() {
+                            selectedTheme = "Light";
+                          });
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 13),
+                          decoration: BoxDecoration(
+                            color: selectedTheme == "Light"
+                                ? Colors.cyanAccent
+                                : Colors.white.withOpacity(0.12),
+                            borderRadius: BorderRadius.circular(13),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Light",
+                              style: TextStyle(
+                                color: selectedTheme == "Light"
+                                    ? const Color(0xFF061B33)
+                                    : Colors.white70,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(width: 7),
+
+                    // Dark
+                    Expanded(
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(13),
+                        onTap: () {
+                          setState(() {
+                            selectedTheme = "Dark";
+                          });
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 13),
+                          decoration: BoxDecoration(
+                            color: selectedTheme == "Dark"
+                                ? Colors.cyanAccent
+                                : Colors.white.withOpacity(0.12),
+                            borderRadius: BorderRadius.circular(13),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Dark",
+                              style: TextStyle(
+                                color: selectedTheme == "Dark"
+                                    ? const Color(0xFF061B33)
+                                    : Colors.white70,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(width: 7),
+
+                    // 📱 System
+                    Expanded(
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(13),
+                        onTap: () {
+                          setState(() {
+                            selectedTheme = "System";
+                          });
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 13),
+                          decoration: BoxDecoration(
+                            color: selectedTheme == "System"
+                                ? Colors.cyanAccent
+                                : Colors.white.withOpacity(0.12),
+                            borderRadius: BorderRadius.circular(13),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "System",
+                              style: TextStyle(
+                                color: selectedTheme == "System"
+                                    ? const Color(0xFF061B33)
+                                    : Colors.white70,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+
+              
             ],
           ),
         ),
